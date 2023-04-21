94f1cde672e055c87eb5407b720b3ab64cbea736
László Ivicz <ivicz.laszlo.dr@gmail.com>
2023-04-21 08:06:10 +0200

    login layout fix

--- src/components/Login.tsx
+++ src/components/Login.tsx
@@ -1,32 +1,24 @@
 function Login() {
   return (
     <>
-      <div className="container">
-        <div className="row justify-content-center">
-          <div className="text-center">Login</div>
-          <div className="row justify-content-center">
-            <div className="col-3">
-              <label htmlFor="userName" className="form-label">
-                User name
-              </label>
-              <input type="text" className="form-control" id="userName" />
-            </div>
-          </div>
-          <div className="row justify-content-center">
-            <div className="col-3">
-              <label htmlFor="password" className="form-label">
-                Password
-              </label>
-              <input type="password" className="form-control" id="password" />
-            </div>
-          </div>
-          <div className="row justify-content-center">
-            <div className="col-3">
-              <button type="button" className="btn btn-primary gap-2">
-                Log in
-              </button>
-            </div>
-          </div>
+      <div className="col-3 mx-auto">
+        <div className="text-center display-6">Login</div>
+        <div className="justify-content-center">
+          <label htmlFor="userName" className="form-label">
+            User name
+          </label>
+          <input type="text" className="form-control" id="userName" />
+        </div>
+        <div className="justify-content-center">
+          <label htmlFor="password" className="form-label">
+            Password
+          </label>
+          <input type="password" className="form-control" id="password" />
+        </div>
+        <div className="col-3 mx-auto d-flex justify-content-center pt-4">
+          <button type="button" className="btn btn-primary gap-2">
+            Log in
+          </button>
         </div>
       </div>
     </>
